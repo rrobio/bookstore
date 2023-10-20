15dd630aff2d32a8398ef36bcb6365cf5be35f4b
rrobio <orlandinirobi@gmail.com>
2023-10-20 09:41:30 +0200

    book store route

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
@@ -10,6 +11,14 @@ class BookController extends Controller
 {
     //
     public function store(Request $request): RedirectResponse {
+        $this->validateWithBag('bookAdd', $request, [
+            'title' => ['required', 'max:255'],
+            'publication' => ['required'],
+            'author_id' => ['required', 'exists:authors,id'],
+            'price' => ['required', 'numeric'],
+        ]);
+
+        Book::create($request->all());
 
         return Redirect::route('dashboard')->with('status', 'book-added');
     }
