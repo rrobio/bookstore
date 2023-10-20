ed79f7e424c74be1455888f30fbf402890bb370e
rrobio <orlandinirobi@gmail.com>
2023-10-20 13:17:40 +0200

    image card remove unnecessary min width

--- resources/views/components/image-card.blade.php
+++ resources/views/components/image-card.blade.php
@@ -2,7 +2,7 @@
     'img',
     'alt'
 ])
-<div class="relative flex shrink-0 bg-gray-100 hover:bg-indigo-50 rounded-lg min-w-[350] ">
+<div class="relative flex shrink-0 bg-gray-100 hover:bg-indigo-50 rounded-lg w-full ">
     <div class="relative m-0 w-2/5 shrink-0">
         <img src="https://picsum.photos/id/{{$img}}/150/300" alt="{{$alt}}"
              class=" rounded-md mx-auto">
