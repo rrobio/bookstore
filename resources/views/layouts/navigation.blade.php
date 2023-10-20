0eeaee81bd50c6e73f3151676a349d1ca7bc9ba6
rrobio <orlandinirobi@gmail.com>
2023-10-20 10:06:22 +0200

    style changes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -77,12 +77,14 @@
             @endauth
             @guest
                 @if (Route::has('login'))
-                    <x-nav-link :href="route('login')" :active="request()->routeIs('dashboard')">
-                        Login
-                    </x-nav-link>
-                    <x-nav-link :href="route('register')" :active="request()->routeIs('dashboard')">
-                        Register
-                    </x-nav-link>
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-nav-link :href="route('login')" :active="request()->routeIs('dashboard')">
+                            Login
+                        </x-nav-link>
+                        <x-nav-link :href="route('register')" :active="request()->routeIs('dashboard')">
+                            Register
+                        </x-nav-link>
+                    </div>
                 @endif
             @endguest
         </div>
