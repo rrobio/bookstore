229bb7ea2db634b6ad798f245d31a21fdbeacfb4
rrobio <orlandinirobi@gmail.com>
2023-10-20 09:42:14 +0200

    add api routes to get books and authors

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,28 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('authors', function () {
+    return response()->json(['authors' => Author::all()->setHidden(['created_at', 'updated_at'])]);
+});
+
+Route::get('authors/{id}', function (string $id) {
+    $validator = Validator::make(['id' => $id], [
+        'id' => ['required', 'exists:authors,id']
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 400);
+    }
+
+    return response()->json([
+       'author' => Author::where('id', (int)$id)->first()->makeHidden(['created_at', 'updated_at'])
+    ]);
+})->where(['id' => '[0-9]+']);
+
+Route::get('books', function () {
+    return response()->json(['books' => Book::all()->setHidden(['created_at', 'updated_at'])]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
